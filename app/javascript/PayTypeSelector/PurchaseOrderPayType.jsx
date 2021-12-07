0be42b2bed10c4e808875c13c4c29ffd526285dc
Nicholas Kirby <nkirby@nxtsoft.com>
2021-12-07 15:55:02 -0500

    fix closing

--- app/javascript/PayTypeSelector/PurchaseOrderPayType.jsx
+++ app/javascript/PayTypeSelector/PurchaseOrderPayType.jsx
@@ -6,7 +6,7 @@ class PurchaseOrderPayType extends React.Component {
 			<div>
 				<div className="field">
 					<label htmlFor="order_po_number">PO #</label>
-					<input type="password" name="order[po_number]" id="order_po_number"
+					<input type="password" name="order[po_number]" id="order_po_number" />
 				</div>
 			</div>
 		}
