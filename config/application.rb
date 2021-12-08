5d3568ff49930c6146021a2d1b48fb6b118482c7
Nicholas Kirby <nkirby@nxtsoft.com>
2021-12-08 01:10:35 -0500

    add controller logic for pay components

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Depot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.filter_parameter += [:credit_card_number]
   end
 end
